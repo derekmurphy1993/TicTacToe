6f1dada86643df826a3cc06ff04d3618dce4619f
Derek <robinmurphy@Robins-MacBook-Pro.local>
2019-11-14 17:30:59 -0500

    Tweaked Code
    Small changes saved to deploy

--- assets/scripts/logic-scripts/events.js
+++ assets/scripts/logic-scripts/events.js
@@ -8,33 +8,29 @@ let currentPlayer = true
 
 const onPlayerMove = event => {
   // if target does not have the class "taken", add the class and claim the tile
+
   if (!($(event.target).hasClass('taken'))) {
-    $(event.target)
+    $(event.target).addClass('taken')
     $(ui.playerMoveSuccess)
     if (currentPlayer === true) {
-      $(event.target).css('background-color', 'red').addClass('x').text('x').addClass('taken')
-      currentPlayer = !currentPlayer
+      $(event.target).css('background-color', 'red').addClass('x').text('x')
+      $('#playerIcon').css('background-color', 'blue').text(`Player O's turn!`)
     } else {
-      $(event.target).css('background-color', 'blue').addClass('o').text('o').addClass('taken')
-      currentPlayer = !currentPlayer
+      $(event.target).css('background-color', 'blue').addClass('o').text('o')
+      $('#playerIcon').css('background-color', 'red').text(`Player X's turn!`)
     }
+    currentPlayer = !currentPlayer
   } else {
     $(ui.playerMoveFailure)
   }
 }
 
-// player switch functionality
-
-// var flag = false;
-// function flip(){
-//   flag = !flag;
-//   return flag;
-// }
-
 // to steamline the module.exports
 const addHandlers = event => {
-  $('.col-4').on('click', onPlayerMove)
+  $('.box').on('click', onPlayerMove)
 }
+
+// is game over
 
 module.exports = {
   addHandlers
