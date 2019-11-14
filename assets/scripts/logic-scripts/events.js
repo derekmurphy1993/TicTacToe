ef81203579d3bda979e8c8881863266d1869150f
Derek <robinmurphy@Robins-MacBook-Pro.local>
2019-11-14 16:20:27 -0500

    Added player switch functionality
    Player will switch when a move is successfully made.

--- assets/scripts/logic-scripts/events.js
+++ assets/scripts/logic-scripts/events.js
@@ -4,17 +4,32 @@
 // const api = require('./api')
 // const store = require('../store.js')
 const ui = require('./ui')
+let currentPlayer = true
 
 const onPlayerMove = event => {
   // if target does not have the class "taken", add the class and claim the tile
   if (!($(event.target).hasClass('taken'))) {
     $(event.target)
     $(ui.playerMoveSuccess)
-    $(event.target).css('background-color', 'red').addClass('x').text('x').addClass('taken')
+    if (currentPlayer === true) {
+      $(event.target).css('background-color', 'red').addClass('x').text('x').addClass('taken')
+      currentPlayer = !currentPlayer
+    } else {
+      $(event.target).css('background-color', 'blue').addClass('o').text('o').addClass('taken')
+      currentPlayer = !currentPlayer
+    }
   } else {
     $(ui.playerMoveFailure)
   }
 }
+
+// player switch functionality
+
+// var flag = false;
+// function flip(){
+//   flag = !flag;
+//   return flag;
+// }
 
 // to steamline the module.exports
 const addHandlers = event => {
