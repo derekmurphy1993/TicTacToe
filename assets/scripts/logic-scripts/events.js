76f644f790043de04b0b8c530cfd17cd5ee8a903
Derek Murphy <derekmurphy1993@gmail.com>
2019-11-14 18:20:06 -0500

    End of Day Commit
    Slight refactoring and reordering of code, no functionality change

--- assets/scripts/logic-scripts/events.js
+++ assets/scripts/logic-scripts/events.js
@@ -9,27 +9,26 @@ let currentPlayer = true
 const onPlayerMove = event => {
   // if target does not have the class "taken", add the class and claim the tile
   if (!($(event.target).hasClass('taken'))) {
-    $(event.target)
     $(ui.playerMoveSuccess)
+    console.log(gameBoard)
+    currentPlayer = !currentPlayer
     if (currentPlayer === true) {
-      $(event.target).css('background-color', 'red').addClass('x').text('x').addClass('taken')
-      currentPlayer = !currentPlayer
+      $(event.target).css('background-color', 'red').text('x').addClass('taken')
+      $('#playerIcon').css('background-color', 'blue')
+      $('#message').text(`It's now Player O's turn.`)
     } else {
-      $(event.target).css('background-color', 'blue').addClass('o').text('o').addClass('taken')
-      currentPlayer = !currentPlayer
+      $(event.target).css('background-color', 'blue').text('o').addClass('taken')
+      $('#playerIcon').css('background-color', 'red')
+      $('#message').text(`It's now Player X's turn.`)
     }
   } else {
     $(ui.playerMoveFailure)
   }
 }
 
-// player switch functionality
+// gameboard
 
-// var flag = false;
-// function flip(){
-//   flag = !flag;
-//   return flag;
-// }
+const gameBoard = ['', '', '', '', '', '', '', '', '']
 
 // to steamline the module.exports
 const addHandlers = event => {
