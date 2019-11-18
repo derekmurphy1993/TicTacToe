eef56285f7c5bf1a135d18cb8d76a050cc837411
Derek Murphy <derekmurphy1993@gmail.com>
2019-11-18 11:16:11 -0500

    New Game called from api
    Able to get new game called from the API

--- assets/scripts/logic-scripts/api.js
+++ assets/scripts/logic-scripts/api.js
@@ -1,49 +1,7 @@
 'use strict'
 // TALK w API
-
-const config = require('../config.js')
-const store = require('../store')
-
-const signUp = formData => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-up',
-    method: 'POST',
-    data: formData
-  })
-}
-
-const signIn = formData => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-in',
-    method: 'POST',
-    data: formData
-  })
-}
-
-const changePassword = formData => {
-  return $.ajax({
-    url: config.apiUrl + '/change-password',
-    method: 'PATCH',
-    headers: {
-      Authorization: `Token token=${store.user.token}`
-    },
-    data: formData
-  })
-}
-
-const signOut = formData => {
-  return $.ajax({
-    url: config.apiUrl + '/sign-out',
-    method: 'DELETE',
-    headers: {
-      Authorization: `Token token=${store.user.token}`
-    }
-  })
-}
+// const config = require('../config.js')
+// const store = require('../store')
 
 module.exports = {
-  signUp,
-  signIn,
-  changePassword,
-  signOut
 }
