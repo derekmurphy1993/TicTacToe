f3c4eb025a637c77bfb25efdf96c7a21bf3881bd
Derek <robinmurphy@Robins-MacBook-Pro.local>
2019-11-18 15:45:24 -0500

    Isolating problem with INDEX
    Isolating problem w index

--- assets/scripts/logic-scripts/events.js
+++ assets/scripts/logic-scripts/events.js
@@ -3,24 +3,27 @@
 // const api = require('./api')
 // const getFormFields = require('../../../lib/get-form-fields')
 const ui = require('./ui')
-const apiScript = require('../api-scripts/ui')
+const apiScript = require('../api-scripts/api')
 let currentPlayer = true
 
 const onPlayerMove = event => {
 // if target does not have the class "taken", add the class and claim the tile
   if (!($(event.target).hasClass('taken'))) {
     $(ui.playerMoveSuccess)
+
+    // HOW DO I FIND THE CURRENT SQUARES INDEX??
+    console.log($('#div.box').index())
+
     if (currentPlayer === true) {
       $(event.target).css('background-color', 'red').text('x').addClass('taken')
       $('#playerIcon').css('background-color', 'blue')
       $('#feedback').text(`It's now Player O's turn.`)
-      apiScript.updateGame()
+      apiScript.updateGame(2, 'x')
     } else {
       $(event.target).css('background-color', 'blue').text('o').addClass('taken')
       $('#playerIcon').css('background-color', 'red')
       $('#feedback').text(`It's now Player X's turn.`)
     }
-    console.log($(event.target).attr('ID'))
     currentPlayer = !currentPlayer
   } else {
     $(ui.playerMoveFailure)
@@ -29,7 +32,7 @@ const onPlayerMove = event => {
 
 // to steamline the module.exports
 const addHandlers = event => {
-  $('.col-4').on('click', onPlayerMove)
+  $('.box').on('click', onPlayerMove)
 }
 
 module.exports = {
